25475c949727f0961a576eba72c6bb9a5091697b
insane4l <43768644+insane4l@users.noreply.github.com>
2021-09-23 12:25:51 +0300

    Minor fixes

--- simple-image-accordion/script.js
+++ simple-image-accordion/script.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict'
 
+    function slidesPlugin(initialActiveSlide = 1) {
+        const items = document.querySelectorAll('.accordion__item');
 
-    const items = document.querySelectorAll('.accordion__item');
+        items[initialActiveSlide].classList.add('accordion__item_active')
 
-    for (const item of items) {
-        item.addEventListener('click', () => {
-            removeActiveClasses();
-            item.classList.add('accordion__item_active');
-        })
+        for (const item of items) {
+            item.addEventListener('click', () => {
+                removeActiveClasses();
+                item.classList.add('accordion__item_active');
+            })
+        }
+
+        const removeActiveClasses = () => {
+            items.forEach((item) => {
+                item.classList.remove('accordion__item_active');
+            })
+        }
     }
-
-    const removeActiveClasses = () => {
-        items.forEach((item) => {
-            item.classList.remove('accordion__item_active');
-        })
-    }
-
+    slidesPlugin()
 })
