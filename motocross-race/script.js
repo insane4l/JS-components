0a15302996baaaf6b8176aa473582b0e04134217
insane4l <43768644+insane4l@users.noreply.github.com>
2021-10-11 14:01:23 +0300

    Race created (start race - non-repetitive random speeds for each rider, finish race - pedestal with top 3 winners)

--- motocross-race/script.js
+++ motocross-race/script.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    const gameWrapper = document.querySelector('#game-wrapper');
+    const startRaceBtn = document.querySelector('#start-btn');
+    const raceTrack = document.querySelector('#race-track');
+    const winnersPedestal = document.querySelector('#pedestal-places');
     const ridersListItems = document.querySelectorAll('.start__section .rider__placeholder');
     const placeholderInputs = document.querySelectorAll('.rider__placeholder-input');
 
-    const gameWrapper = document.querySelector('#game-wrapper');
-    const raceTrack = document.querySelector('#race-track');
-    const startRaceBtn = document.querySelector('#start-btn');
 
-    let selectedRiders;
+    let selectedRiders, raceDuration;
+    let raceResults = []; // all riders speed
+    let winners = []; // main 3 places (speed)
+
+
 
     // SELECT RIDERS AND SET RIDERS NAMES
     for (item of ridersListItems) {
@@ -65,6 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
         showSection(2);
         readyToRace();
         startRace();
+        finishRace();
     }
 
     function readyToRace() {
@@ -72,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function() {
         for (let i = 0; i < ridersCount; i++) {
             createRiderRow(selectedRiders[i]);
         }
-        setRidersSpeed(5, 15)
+        raceResults = setRidersSpeed(selectedRiders, 5, 15)
     }
 
     function createRiderRow(rider) {
@@ -83,9 +89,11 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function startRace() {
+        const trackLength = raceTrack.clientWidth;
+        const riderWidth = selectedRiders[0].clientWidth;
         setTimeout( () => {
             selectedRiders.forEach( (rider) => {
-                rider.style.transform = `translateX(${888}px)`
+                rider.style.transform = `translateX(${trackLength - riderWidth}px)`
             })
         }, 0)
         // setTimeout( () => {
@@ -99,17 +107,88 @@ document.addEventListener('DOMContentLoaded', function() {
         // setTimeout( () => {}, 200)
     }
 
-    function setRidersSpeed(min, max) {
-        selectedRiders.forEach( (rider) => {
-            const randomSpeed = getRandomNumber(min, max);
-            rider.style.transition = `all ${randomSpeed}s linear`
-        })
+    function setRidersSpeed(riders, min, max) { // TODO: 1 speed value can be used only by one rider (cannot be repeated)
+
+        let ridersSpeed = [];
+        while (ridersSpeed.length < riders.length) {
+            const speed = getRandomNumber(min, max);
+            if (ridersSpeed.indexOf(speed) == -1) {
+                ridersSpeed.push(speed);
+            } 
+        }
+
+        for (let i = 0; i < riders.length; i++) {
+            riders[i].style.transition = `all ${ridersSpeed[i]}s linear`;
+        }
+
+        return ridersSpeed
+
+        // selectedRiders.forEach( (rider) => {
+        //     const randomSpeed = getRandomNumber(min, max);
+        //     raceResults.push(randomSpeed);
+        //     rider.style.transition = `all ${randomSpeed}s linear`
+        // })
     }
 
 
 
 
-    // FINISH PEDESTAL
+    // FINISH
+
+    function finishRace() {
+        winners = getWinners(raceResults, selectedRiders);
+        raceDuration = getRaceDuration(raceResults) * 1000
+
+        setTimeout( () => {
+            showWinners(winners);
+        }, raceDuration);
+    }
+
+
+    // function getWinners(arr) {
+        
+    //     console.log(raceResults);
+    //     const firstPlaceRiderIndex = arr.indexOf( Math.min.apply(Math, arr) );
+    //     winners.push(firstPlaceRiderIndex);
+    //     arr[firstPlaceRiderIndex] = 999;
+
+    //     const secondPlaceRiderIndex = arr.indexOf( Math.min.apply(Math, arr) );
+    //     winners.push(secondPlaceRiderIndex);
+    //     arr[secondPlaceRiderIndex] = 999;
+
+    //     const thirdPlaceRiderIndex = arr.indexOf( Math.min.apply(Math, arr) );
+    //     winners.push(thirdPlaceRiderIndex);
+        
+    //     console.log(winners);
+    // }
+
+    function getWinners(results, riders) {
+        let sorted = [...results].sort( function(a, b) {return a - b} );
+
+        const firstPlace = results.indexOf(sorted[0]);
+        const secondPlace = results.indexOf(sorted[1]);
+        const thirdPlace = results.indexOf(sorted[2]);
+        debugger;
+        return [ riders[secondPlace], riders[firstPlace], riders[thirdPlace] ];
+    }
+
+    function getRaceDuration(results) {
+        const sortedResults = [...results].sort( function(a, b) {return b - a} );
+        return sortedResults[0]; // min rider speed
+    }
+
+    function showWinners(riders) {
+        showSection(3);
+
+        riders.forEach( (el) => {
+            el.style.transition = 'all 3s linear'; // todo: riders animation when winners pedestal will show
+            el.style.transform = `translateX(0)`;
+        })
+        console.log(riders);
+        winnersPedestal.appendChild(riders[0]);
+        winnersPedestal.appendChild(riders[1]);
+        winnersPedestal.appendChild(riders[2]);
+    }
 
 
 
